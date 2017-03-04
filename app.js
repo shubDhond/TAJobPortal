b56de323897ef82ee89121264782ae45699d58e0
Shrey Mahendru <shreymahendru@gmail.com>
2017-03-04 18:46:55 -0500

    added resource specific routers

---
+++ app.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var coordinator = require('./routes/coordinator');
+var course = require('./routes/course');
+var assignments = require('./routes/assignments');
+var posting = require('./routes/posting');
+
+var app = express();
+
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', index);
+app.use('/coordinator', coordinator);
+app.use('/course', course);
+app.use('/assignment', assignments);
+app.use('/posting', posting);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send('You got error: ' + err.status);
+});
+
+module.exports = app;
--- routes/posting.js
+++ routes/posting.js
@@ -1,9 +1,12 @@
+/**
+ * Created by shrey.mahendru on 2017-03-04.
+ */
 var express = require('express');
 var router = express.Router();
 
-/* GET coordinators. */
+/* GET postings. */
 router.get('/', function(req, res, next) {
-  res.send('This is where you get coordinators.');
+    res.send('This is where you get postings.');
 });
 
 module.exports = router;
