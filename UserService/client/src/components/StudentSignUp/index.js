2d85961f8964e69ca96486d6b669ab5b47a38737
davjhan <mr.davidjhan@gmail.com>
2017-03-15 00:37:26 -0400

    Finished styling splash page

--- UserService/client/src/components/StudentSignUp/index.js
+++ UserService/client/src/components/StudentSignUp/index.js
@@ -1,111 +1,79 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import './style.css';
-import {Link} from "react-router";
-import {Form, FormControl, Jumbotron, Col, Row, Grid, Button} from "react-bootstrap";
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import "./style.css";
+import {Button, Col, Form, FormControl, Grid, Jumbotron, Row} from "react-bootstrap";
 
 @connect((store) => {
-  return {
-    user: store.user
-  }
+    return {
+        user: store.user
+    }
 })
 class App extends Component {
-  render() {
-      const jumboStyle ={
-          height:"100%",
-          background:"none"
-      };
+    render() {
+        const jumboStyle = {
+            height: "100%",
+            background: "none"
+        };
 
-    return (
-      <Grid fluid style={{height:"100%", padding:0}}>
-          <Row>
+        return (
+            <Grid fluid style={{height: "100%"}}>
+                <Row style={{padding:64}}>
+                    <Col xs={12} sm="9">
+                        <Jumbotron style={jumboStyle}>
+                            <h1 style={{marginBottom: 16, marginTop: 64}}>Apply to be a TA</h1>
+                            <h2 style={{marginBottom: 48}}>Lorem Ipsum Lorem Ipsum</h2>
 
-              <Col xs={12} sm="6">
-                  <Jumbotron style={jumboStyle}>
-                      <h1 style={{marginBottom:16,marginTop:64}}>Apply to be a TA</h1>
-                      <h2 style={{marginBottom:48}}>Lorem Ipsum Lorem Ipsum</h2>
+                        </Jumbotron>
+                    </Col>
+                    <Col xs={12} sm="3">
+                        <Form horizontal className="card" >
+                            <Row>
+                                <Col xs={12} style={{marginBottom:24}}>
+                                    <h2 style={{teaxtAlign:"center", margin:0}}> Sign In</h2>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs={12}>
+                                    <h6> EMAIL</h6>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs={12}>
+                                    <FormControl type="text" placeholder="Password"/>
+                                </Col>
+                            </Row>
+                            <br />
+                            <Row>
+                                <Col xs={12}>
+                                    <h6> PASSWORD</h6>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs={12}>
+                                    <FormControl type="password" placeholder="Password"/>
+                                </Col>
+                            </Row>
+                            <br />
+                            <Row>
+                                <Col xs={12}>
+                                    <Button block  bsStyle="primary"
+                                            bsSize="large" href="/app/profile">Sign In</Button>
+                                </Col>
+                            </Row>
+                            <br/>
+                            <Row>
+                                <Col xs={12}>
+                                    <a className="see-more centered"><h5>Create an Account</h5></a>
+                                </Col>
+                            </Row>
+                        </Form>
+                    </Col>
+                </Row>
 
-                  </Jumbotron>
-              </Col>
-              <Col xs={12} sm="6">
-              <Form horizontal className="card">
-                  <Row>
-                      <Col xs={4} >
-                          <h6> FIRST NAME</h6>
-                      </Col>
-                      <Col xs={4}>
-                          <h6>LAST NAME</h6>
-                      </Col>
-                      <Col xs={4} >
-                          <h6>STUDENT NUMBER</h6>
-                      </Col>
-                  </Row>
-
-                  <Row>
-                      <Col xs={4} >
-                          <FormControl type="text" placeholder="First Name"/>
-                      </Col>
-                      <Col xs={4}>
-                          <FormControl type="text" placeholder="Last Name"/>
-                      </Col>
-                      <Col xs={4} >
-                          <FormControl type="text" pattern="[0-9]" placeholder="Student Number"/>
-                      </Col>
-                  </Row>
-
-                  <br />
-
-                  {/*<Row>*/}
-                  {/*<Col xs={12} >*/}
-                  {/*<FormControl type="text" pattern="[0-9]" placeholder="Something else"/>*/}
-                  {/*</Col>*/}
-                  {/*</Row>*/}
-
-                  <Row>
-                      <Col xs={12} >
-                          <FormControl type="text" pattern="[0-9]" placeholder="blahblah"/>
-                      </Col>
-                  </Row>
-
-                  <br />
-
-                  <Row>
-                      <Col xs={3} >
-                          <h6>ABOUT YOU</h6>
-                      </Col>
-                  </Row>
-                  <Row>
-                      <Col xs={12} >
-                          <FormControl style={{height:128}} componentClass="textarea" placeholder="About you"/>
-                      </Col>
-                  </Row>
-
-                  <br />
-
-                  <Row>
-                      <Col xs={3} >
-                          <h6>RESUME</h6>
-                      </Col>
-                      <Col >
-                          <FormControl type="file"/>
-                      </Col>
-                  </Row>
-
-                  <br />
-                  <Row>
-                      <Col xs={12} >
-                          <Button className="right-align" bsStyle="primary" bsSize="large">Submit</Button>
-                      </Col>
-                  </Row>
-
-              </Form>
-              </Col>
-          </Row>
-
-          {this.props.children}
-      </Grid>
-    );
-  }
+                {this.props.children}
+            </Grid>
+        );
+    }
 }
 
 export default App;
