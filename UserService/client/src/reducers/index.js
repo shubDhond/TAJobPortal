20bbff371675df4d4f3da592f671f038c7a07fc8
davjhan <mr.davidjhan@gmail.com>
2017-03-15 12:47:57 -0400

    Merge branch 'ta-app-inbox' of https://github.com/csc302-2017-spring/proj-Awol into apply-styling
    # Conflicts:
    #	UserService/client/src/components/pages/Inbox.js
    #	UserService/client/src/reducers/index.js

--- UserService/client/src/reducers/index.js
+++ UserService/client/src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
-
+import InboxReducer from './reducer-inbox';
 import listings from "./listingsReducer";
 
 export default combineReducers({
-    listings,
+	listings,
+	inbox: InboxReducer
 });
