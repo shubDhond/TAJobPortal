59fa3efb785604c57b93cd779e3b0a3ff33a2c1d
davjhan <mr.davidjhan@gmail.com>
2017-03-14 23:26:57 -0400

    Added link styling and more card styling to jobs

--- UserService/client/src/components/pages/Layout.js
+++ UserService/client/src/components/pages/Layout.js
@@ -14,7 +14,6 @@ export default class Layout extends React.Component {
 
     render() {
         return (
-
             <div >
                 <Header/>
                 <Grid >
--- UserService/client/src/components/pages/Listing.js
+++ UserService/client/src/components/pages/Listing.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Col, Row} from "react-bootstrap";
+import {Glyphicon, Col, Row} from "react-bootstrap";
 import ListItem from "./views/ListItem";
 import {Link} from "react-router";
 
@@ -17,27 +17,32 @@ export default class Listing extends React.Component {
     render() {
         return (
             <ListItem>
-                <Row>
+                <Row style={{marginBottom: 24}}>
                     <Col xs={4}>
-                    <h2 style=""{{padding:16}}>
-                        {this.state.title}
-                    </h2>
-                    </Col>
-                    <Col xs={6} right>
-                    </Col>
-                </Row>
 
-                <Row>
-                    <p>
-                    {this.state.description}
-                    </p>
+                        <a> <h2 style={{margin: 0 ,fontWeight:600}}>
+                            {this.state.title}
+                        </h2></a>
+                    </Col>
+                    {/*<Col xs={8} right>*/}
+                        {/*(2) Spaces Left*/}
+                    {/*</Col>*/}
+
                 </Row>
                 <Row>
-                    Apply by: {this.state.deadline}
+                    <Col xs={12}>
+                        <p>
+                            {this.state.description}
+                        </p>
+                        <br/>
+                        <p>
+                            Apply by 02-03-2017
+                        </p>
+                    </Col>
                 </Row>
                 <Row>
-                    <Col xs={2} xsOffset={10}>
-                        <a><Link>View</Link></a>
+                    <Col xs={2} xsOffset={10} >
+                        <a className="see-more right-align"><Link>View<Glyphicon glyph="chevron-right" /></Link></a>
                     </Col>
                 </Row>
             </ListItem>
