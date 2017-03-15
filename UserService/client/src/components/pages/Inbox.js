e6dbf39927a3b7f17b3006bf51414159bf9a9378
davjhan <mr.davidjhan@gmail.com>
2017-03-15 12:56:50 -0400

    Finished simple styling of ta-inbox

--- UserService/client/src/components/pages/Inbox.js
+++ UserService/client/src/components/pages/Inbox.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Table } from 'react-bootstrap';
+
+import InboxList from './inbox_list';
+
 
 export default class Inbox extends React.Component {
-  render() {
-    return (
-      <div>
-      </div>
-    );
-  }
+  	render() {
+    	return (
+      	<div className="card">
+        	 <InboxList />
+      	</div>
+    	);
+  	}
 }
---
+++ UserService/client/src/reducers/reducer-inbox.js
@@ -0,0 +1,19 @@
+export default function() {
+	return [
+		{
+			posting_id: 1,
+			course: "csc301",
+			status: "Pending"
+		},
+		{
+			posting_id: 2,
+			course: "csc302",
+			status: "Accepted"
+		},
+		{
+			posting_id: 3,
+			course: "csc303",
+			status: "Rejected"
+		}
+	]
+}
