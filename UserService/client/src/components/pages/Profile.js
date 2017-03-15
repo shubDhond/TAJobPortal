278253c5f35d6dfd4ed514ad351fadfbdaff72d9
davjhan <mr.davidjhan@gmail.com>
2017-03-15 00:04:15 -0400

    moved student number up a line in Profile

--- UserService/client/src/components/pages/Profile.js
+++ UserService/client/src/components/pages/Profile.js
@@ -9,34 +9,40 @@ export default class Profile extends React.Component {
 
                 <Form horizontal className="card">
                     <Row>
-                        <Col xs={6} >
+                        <Col xs={4} >
                             <h6> FIRST NAME</h6>
                         </Col>
-                        <Col xs={6}>
+                        <Col xs={4}>
                             <h6>LAST NAME</h6>
                         </Col>
-                    </Row>
-
-                    <Row>
-                        <Col xs={6} >
-                            <FormControl type="text" placeholder="First Name"/>
-                        </Col>
-                        <Col xs={6}>
-                            <FormControl type="text" placeholder="Last Name"/>
-                        </Col>
-                    </Row>
-
-                    <br />
-
-                    <Row>
-                        <Col xs={3} >
+                        <Col xs={4} >
                             <h6>STUDENT NUMBER</h6>
                         </Col>
                     </Row>
 
                     <Row>
-                        <Col xs={12} >
+                        <Col xs={4} >
+                            <FormControl type="text" placeholder="First Name"/>
+                        </Col>
+                        <Col xs={4}>
+                            <FormControl type="text" placeholder="Last Name"/>
+                        </Col>
+                        <Col xs={4} >
                             <FormControl type="text" pattern="[0-9]" placeholder="Student Number"/>
+                        </Col>
+                    </Row>
+
+                    <br />
+
+                    {/*<Row>*/}
+                        {/*<Col xs={12} >*/}
+                            {/*<FormControl type="text" pattern="[0-9]" placeholder="Something else"/>*/}
+                        {/*</Col>*/}
+                    {/*</Row>*/}
+
+                    <Row>
+                        <Col xs={12} >
+                            <FormControl type="text" pattern="[0-9]" placeholder="blahblah"/>
                         </Col>
                     </Row>
 
