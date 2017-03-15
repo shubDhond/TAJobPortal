55dbe1f337eece6921c47e5181c5118e378f02c9
racormaz <rasca1000@hotmail.com>
2017-03-15 13:58:00 -0400

    modified the application page and added route to it

--- UserService/client/src/components/pages/Application.js
+++ UserService/client/src/components/pages/Application.js
@@ -1,12 +1,29 @@
 import React from "react";
 import {Navbar} from "react-bootstrap";
 
+import { Button,Grid,Row,Col } from "react-bootstrap";
+import { browserHistory } from "react-router";
+
 export default class Application extends React.Component {
   render() {
     return (
-      <Navbar fixedTop="True">
-        <Navbar.Link>Application</Navbar.Link>
-      </Navbar>
+      <Grid>
+        <Row>
+          <Col xs={12} md={12}><Button onClick={browserHistory.goBack}>Back</Button></Col>
+        </Row>
+        <Row>
+          <Col xs={12} md={8}><h2>Course Name</h2></Col>
+          <Col xs={12} md={4}><h2>Apply by:</h2></Col>
+        </Row>
+
+        <Row>
+          <Col xs={12} md={12}>Description of the course</Col>
+        </Row>
+
+        <Row>
+          <Col xs={12}><Button bsStyle="success" bsSize="large" block>Apply Now</Button></Col>
+        </Row>
+      </Grid>
     );
   }
 }
--- UserService/client/src/routes.js
+++ UserService/client/src/routes.js
@@ -6,7 +6,7 @@ import Layout from "./components/pages/Layout";
 import Jobs from "./components/pages/Jobs";
 import Inbox from "./components/pages/Inbox";
 import Profile from "./components/pages/Profile";
-// import Application from "./components/pages/Application";
+import Application from "./components/pages/Application";
 
 const Routes = (props) => (
   <Router {...props}>
@@ -16,6 +16,7 @@ const Routes = (props) => (
                 <Route path="/app/profile" component={Profile} pageTitle="Open Jobs"/>
                 <Route path="/app/jobs" component={Jobs} pageTitle="Open Jobs"/>
                 <Route path="/app/inbox" component={Inbox} pageTitle="Inbox"/>
+                <Route path="/app/application" component={Application} pageTitle="Application"/>
 
     </Route>
   </Router>
