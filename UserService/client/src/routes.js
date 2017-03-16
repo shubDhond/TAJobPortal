69d56cc3dce0d556d760da4e5b81062c220cd73c
waynellin <wayne96160@gmail.com>
2017-03-15 20:03:35 -0400

    added ta coord app components

--- UserService/client/src/routes.js
+++ UserService/client/src/routes.js
@@ -6,6 +6,8 @@ import Layout from "./components/pages/Layout";
 import Jobs from "./components/pages/Jobs";
 import Inbox from "./components/pages/Inbox";
 import Profile from "./components/pages/Profile";
+
+import CoordLayout from "./components/TaCoord/CoordLayout";
 // import Application from "./components/pages/Application";
 
 const Routes = (props) => (
@@ -16,6 +18,9 @@ const Routes = (props) => (
                 <Route path="/app/profile" component={Profile} pageTitle="Open Jobs"/>
                 <Route path="/app/jobs" component={Jobs} pageTitle="Open Jobs"/>
                 <Route path="/app/inbox" component={Inbox} pageTitle="Inbox"/>
+
+    </Route>
+    <Route path="/coord" component={CoordLayout}>
 
     </Route>
   </Router>
