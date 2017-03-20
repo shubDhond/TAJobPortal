935c55b1ad92ef2145d899c1e0f9a207e16f3220
waynellin <wayne96160@gmail.com>
2017-03-20 00:48:04 -0400

    added components for about me and courses

--- UserService/client/src/components/TaCoord/applicant-list.js
+++ UserService/client/src/components/TaCoord/applicant-list.js
@@ -1,48 +1,72 @@
 import React, {Component} from "react";
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
-import {PanelGroup, Panel, Accordion, Well, Image} from 'react-bootstrap';
+import {PanelGroup, Panel, Accordion, Well, Image, Button} from 'react-bootstrap';
 
 class PanelHeader extends Component{
 	render(){
 		return (
 			<div>
 				<Image width="80" height="80" src="https://react-bootstrap.github.io/assets/thumbnail.png" circle />
-				{this.props.name}, {this.props.student_id}
+				{this.props.first_name} {this.props.last_name}, {this.props.student_id}
 			</div>
 		);
 	}
 }
 
-class PanelContent extends Component{
+class AboutMe extends Component{
 	render(){
 		return (
 			<div>
 				About me: <Well>{this.props.content}</Well>
-				Prefered Courses: <Well>{this.props.courses}</Well>
-        	</div>
+			</div>
+		);
+	}
+}
+
+class Courses extends Component{
+	getCourses(){
+		return this.props.courses.map((course) => {
+			return (
+				<Button type="submit" >{course}</Button>
+			);
+		});
+	}
+
+	render(){
+		return (
+			<div>
+				Prefered Courses: 
+				<Well>{this.getCourses()}</Well>
+			</div>
 		);
 	}
 }
 
 class ApplicantList extends Component{
+	constructor(){
+	    super();
+	    this.state = {
+	      search: 'level'
+    	};
+	}
 
 	getApplicants(){
 		return this.props.applicants.map((applicant) => {
 			return (
 				<Panel header=
 					{<div>
-						<PanelHeader name={applicant.first_name} student_id={applicant.student_id} profile_pic={applicant.profile_pic}/>
+						<PanelHeader first_name={applicant.first_name} last_name={applicant.last_name} student_id={applicant.student_id} profile_pic={applicant.profile_pic}/>
 					</div>} 
 					eventKey={applicant.id}>
 					{<div>
-						<PanelContent content={applicant.details} courses={applicant.courses}/>
+						<AboutMe content={applicant.details} />
+						<Courses courses={applicant.courses}/>
 					</div>}
 				</Panel>
 			);
 		});
 	}
-	
 
 	render(){
 		return (
--- UserService/client/src/reducers/reducer-applicants.js
+++ UserService/client/src/reducers/reducer-applicants.js
@@ -5,48 +5,48 @@ export default function() {
 			student_id: 1000123123,
 			first_name: "Larry",
 			last_name: "Jones",
-			details: "abc def ghi jkl mno pqr stu vwx yz",
-			courses: "csc301 csc 302 csc303"
+			details: "Ted ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+			courses: ["csc301", "csc 302", "csc303"]
 		},
 		{
 			id: 2,
 			student_id: 1000234234,
 			first_name: "Kevin",
 			last_name: "Johnson",
-			details: "123 456 789",
-			courses: "csc301 csc 302 csc303"
+			details: "Ted ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+			courses: ["csc301", "csc 302", "csc303"]
 		},
 		{
 			id: 3,
 			student_id: 1000345345,
 			first_name: "Bill",
 			last_name: "James",
-			details: "bc def ghi jkl mno 111 222 333",
-			courses: "csc301 csc 302 csc303"
+			details: "Ted ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+			courses: ["csc301", "csc 302", "csc303"]
 		},
 		{
 			id: 4,
 			student_id: 1000345345,
 			first_name: "Will",
 			last_name: "James",
-			details: "bc def ghi jkl mno 111 222 333",
-			courses: "csc301 csc 302 csc303"
+			details: "Ted ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+			courses: ["csc301", "csc 302", "csc303"]
 		},
 		{
 			id: 5,
 			student_id: 1000345345,
 			first_name: "Ian",
 			last_name: "James",
-			details: "bc def ghi jkl mno 111 222 333",
-			courses: "csc301 csc 302 csc303"
+			details: "Ted ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+			courses: ["csc301", "csc 302", "csc303"]
 		},
 		{
 			id: 6,
 			student_id: 1000345345,
 			first_name: "Jason",
 			last_name: "James",
-			details: "bc def ghi jkl mno 111 222 333",
-			courses: "csc301 csc 302 csc303"
+			details: "Ted ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
+			courses: ["csc301", "csc 302", "csc303"]
 		}
 	]
 }
