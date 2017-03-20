2405026da098003abf39cbf9a7eff2dbee6aedea
chojohn <hoyeon517@gmail.com>
2017-03-20 12:09:10 -0400

    course.js merge error fixed

--- UserService/client/src/components/TaCoord/courses.js
+++ UserService/client/src/components/TaCoord/courses.js
@@ -1,5 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
 import { connect } from "react-redux";
 import { Button, Col, ListGroupItem, ListGroup, Row } from 'react-bootstrap';
 import TaCoordListing from "./TaCoordListing";
@@ -21,48 +20,8 @@ export default class Courses extends React.Component {
         </Row>
         <ListGroup>
           <TaCoordListing />
-=======
-import {connect} from "react-redux";
-import {Button, Col, ListGroup, Row} from "react-bootstrap";
-import Listing from "./TaCoordListing";
-import {fetchListings} from "../../actions/listingsActions";
-import SearchBar from "./SearchBar";
-
-@connect((store) => {
-    return {
-        listings: store.listings.listings
-    };
-})
-
-export default class Courses extends React.Component {
-    componentWillMount() {
-        this.state = {...this.state, listings: this.props.dispatch(fetchListings).payload};
-    }
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            listings: []
-        }
-    }
-
-    render() {
-        return (
-            <Col xs={12} style={{padLeft:14,borderLeft:"1px solid #E0E0E0"}}>
-                <h3>Courses</h3>
-                <Row>
-                    <Col xs={10}>
-                        <SearchBar />
-                    </Col>
-                    <Col xs={2}>
-                        <Button type="submit" bsSize="small" block={true}>New Ad</Button>
-                    </Col>
-                </Row>
-            <ListGroup>
-                <TaCoordListing />
->>>>>>> 2d68edf67dca6d4d4928c989e6ab5916509d8b5a
 		    </ListGroup>
-            </Col>
-        );
-    }
+    	</div>
+  	);
+	}
 }
