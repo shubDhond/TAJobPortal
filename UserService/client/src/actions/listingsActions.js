0bcdb638a0cea01e07d5b4ca9982eccf9a903e68
chojohn <hoyeon517@gmail.com>
2017-03-20 14:29:48 -0400

    merge error fixed

--- UserService/client/src/actions/listingsActions.js
+++ UserService/client/src/actions/listingsActions.js
@@ -1,18 +1,30 @@
-export function fetchListings() {
+function setListings() {
   return {
-    type: "FETCH_LISTINGS_FULFILLED",
-    payload: [
-     {title: "CSC301",
-  description: "This course is an introduction to the theory and practice of large-scale software system design, development, and deployment. Topics include project management; advanced UML; reverse engineering; requirements inspection; verification and validation software architecture; performance modeling and analysis.",
-  deadline: "2017-04-23",
-  status: "ASSIGNED"},
-  {title: "CSC302",
-  description: "This course is an introduction to the theory and practice of large-scale software system design, development, and deployment. Topics include project management; advanced UML; reverse engineering; requirements inspection; verification and validation software architecture; performance modeling and analysis.",
-  deadline: "2017-04-23",
-  status: "ASSIGNED"},
-   {title: "CSC303",
-  description: "This course is an introduction to the theory and practice of large-scale software system design, development, and deployment. Topics include project management; advanced UML; reverse engineering; requirements inspection; verification and validation software architecture; performance modeling and analysis.",
-  deadline: "2017-04-23",
-  status: "UNASSIGNED"}]
+    type: "SET_LISTINGS",
+    payload: {
+      1: {title: "CSC301",
+      description: "This course is an introduction to the theory and practice of large-scale software system design, development, and deployment. Topics include project management; advanced UML; reverse engineering; requirements inspection; verification and validation software architecture; performance modeling and analysis.",
+      deadline: "2017-04-23",
+      status: "ASSIGNED",
+      ranking: null
+    },
+      2: {title: "CSC302",
+      description: "This course is an introduction to the theory and practice of large-scale software system design, development, and deployment. Topics include project management; advanced UML; reverse engineering; requirements inspection; verification and validation software architecture; performance modeling and analysis.",
+      deadline: "2017-04-23",
+      status: "UNASSIGNED",
+      ranking: null},
+      3: {title: "CSC303",
+      description: "This course is an introduction to the theory and practice of large-scale software system design, development, and deployment. Topics include project management; advanced UML; reverse engineering; requirements inspection; verification and validation software architecture; performance modeling and analysis.",
+      deadline: "2017-04-23",
+      status: "ASSIGNED",
+      ranking: null}
+    }
+  }
+}
+
+export function fetchListings() {
+  return function (dispatch) {
+
+    dispatch(setListings())
   }
 }
--- UserService/client/src/reducers/index.js
+++ UserService/client/src/reducers/index.js
@@ -2,14 +2,10 @@ import { combineReducers } from 'redux';
 import InboxReducer from './reducer-inbox';
 import ApplicantReducer from './reducer-applicants';
 import listings from "./listingsReducer";
-import headings from "./headingsReducer";
-import rankings from "./rankingReducer";
 import application from "./applicationReducer";
 
 export default combineReducers({
-    rankings,
-    listings,
-    headings,
+	listings,
 	inbox: InboxReducer,
 	applicants: ApplicantReducer
 
