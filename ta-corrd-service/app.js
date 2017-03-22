dd765dde49bcd065c99b7915a2abee4fec34a450
Shrey Mahendru <shreymahendru@gmail.com>
2017-03-22 19:37:51 -0400

    moved mongo connection in app.js for ta coord service

--- ta-corrd-service/app.js
+++ ta-corrd-service/app.js
@@ -3,7 +3,8 @@ let path = require('path');
 let logger = require('morgan');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
-
+let mongoose = require('mongoose');
+let confog = require('./config');
 let index = require('./routes/index');
 let course = require('./routes/course');
 let assignments = require('./routes/assignments');
@@ -27,6 +28,12 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+/**
+ * Connect to the DB.
+ */
+
+mongoose.connect(config.MONGO_URL + config.MONGO_CLUSTER_NAME);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
