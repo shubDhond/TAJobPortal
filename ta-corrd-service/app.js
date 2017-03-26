52f881f0e082c3426c2e38a73a5917c06b8a93e0
Shrey Mahendru <shreymahendru@gmail.com>
2017-03-25 20:04:07 -0400

    Ta coord cross origin bug fix

--- ta-corrd-service/app.js
+++ ta-corrd-service/app.js
@@ -18,16 +18,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', index);
-app.use('/course', course);
-app.use('/assignment', assignments);
-app.use('/posting', posting);
-
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+app.use('/', index);
+app.use('/course', course);
+app.use('/assignment', assignments);
+app.use('/posting', posting);
+
 
 /**
  * Connect to the DB.
