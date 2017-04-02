7e23e7a16362e38220d57087a40360451cbeaf49
TenzinLama <lamatenzin9@gmail.com>
2017-04-02 16:47:32 -0400

    test changes

--- ta-corrd-service/test/postingtest.js
+++ ta-corrd-service/test/postingtest.js
@@ -4,19 +4,27 @@
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let expect = chai.expect;
+let Client = require('node-rest-client').Client;
+let client = new Client();
+
 let app = require('../app');
 chai.use(chaiHttp);
 
-//basic test for now...
-// describe('posting', function(){
-//   this.timeout(0);
-//   it('should return status 200', function(done){
-//     chai.request(app)
-//     .get('/posting')
-//     .end(function(err,res){
-//       expect(res).to.have.status(200);
-//       done();
-//     });
-//
-//   });
-// });
+basic test for now...
+describe('posting', function(){
+  this.timeout(0);
+  before(function(done){
+    //sign up for a student account and ta account
+    //create a posting
+    
+  });
+  it('should return status 200', function(done){
+    chai.request(app)
+    .get('/posting')
+    .end(function(err,res){
+      expect(res).to.have.status(200);
+      done();
+    });
+
+  });
+});
